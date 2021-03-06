a1a22ba412711c7b0420314f227091d383d38283
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-03-06 18:45:47 +0100

    Implemented do_polling_v2

--- keypad/keypad.py
+++ keypad/keypad.py
@@ -1,4 +1,4 @@
-from GPIOSimulator_v5 import GPIOSimulator
+import GPIOSimulator_v5 as GPIOsim
 from pynput.keyboard import Listener, Key
 import time
 
@@ -9,21 +9,36 @@ def on_release(key):
 class Keypad:
     
     def __init__(self):
-        self.GPIO = GPIOSimulator()
+        self.GPIO = GPIOsim.GPIOSimulator()
         self.key = ''
 
-        self.key_coord = {'1' (3, 7),
-                            '2' (3, 8),
-                            '3' (3, 9),
-                            '4' (4, 7),
-                            '5' (4, 8),
-                            '6' (4, 9),
-                            '7' (5, 7),
-                            '8' (5, 8),
-                            '9' (5, 9),
-                            '' (6, 7),
-                            '0' (6, 8),
-                            '#' (6, 9)}
+        self.key_coord = {'1': (3, 7),
+                            '2': (3, 8),
+                            '3': (3, 9),
+                            '4': (4, 7),
+                            '5': (4, 8),
+                            '6': (4, 9),
+                            '7': (5, 7),
+                            '8': (5, 8),
+                            '9': (5, 9),
+                            '': (6, 7),
+                            '0': (6, 8),
+                            '#': (6, 9)}
+
+        self.inv_key_coord = {
+                            '(3, 7)': '1',
+                            '(3, 8)':'2',
+                            '(3, 9)': '3',
+                            '(4, 7)': '4',
+                            '(4, 8)': '5',
+                            '(4, 9)': '6',
+                            '(5, 7)': '7',
+                            '(5, 8)': '8',
+                            '(5, 9)': '9',
+                            '(6, 7)': '*',
+                            '(6, 8)': '0',
+                            '(6, 9)': '#'
+        }
         
         self.PIN_KEYPAD_ROW_0 = 3
         self.PIN_KEYPAD_ROW_1 = 4
@@ -72,6 +87,27 @@ class Keypad:
             # if self.key == q
             #     condition = True
 
+    def do_polling_v2(self):
+        pressed_key = None
+        while True:
+            for row in GPIOsim.keypad_row_pins:
+                self.set_all_rows_to_low()
+                self.GPIO.output(row, self.GPIO.HIGH)
+                for col in GPIOsim.keypad_col_pins:
+                    if self.GPIO.input(col) == self.GPIO.HIGH:
+                        pressed_key = self.inv_key_coord[str((row, col))]
+            time.sleep(0.01)
+            if pressed_key is not None:
+                self.set_all_rows_to_low()
+                return pressed_key
+
+    def set_all_rows_to_low(self):
+        self.GPIO.output(GPIOsim.PIN_KEYPAD_ROW_0, self.GPIO.LOW)
+        self.GPIO.output(GPIOsim.PIN_KEYPAD_ROW_1, self.GPIO.LOW)
+        self.GPIO.output(GPIOsim.PIN_KEYPAD_ROW_2, self.GPIO.LOW)
+        self.GPIO.output(GPIOsim.PIN_KEYPAD_ROW_3, self.GPIO.LOW)
+
+
     def get_next_signal(self):
         signal = self.do_polling()
 
@@ -79,7 +115,9 @@ class Keypad:
 def main():
     keypad = Keypad()
     keypad.setup()
-    keypad.do_polling()
+    while True:
+        letter = keypad.do_polling_v2()
+        print(letter)
 
 
 if __name__ == "__main__":
